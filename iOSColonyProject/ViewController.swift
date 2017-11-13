11921a22651c2b0f6a845bf8d710ed3b33de0bf7
Seth Walensky <Seth@seths-air.newton.edu>
2017-11-13 12:29:35 -0500

    Initial commit

---
+++ iOSColonyProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSColonyProject
+//
+//  Created by Seth Walensky on 11/13/17.
+//  Copyright © 2017 Seth Walensky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
